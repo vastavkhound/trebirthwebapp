90475b7f2dfd68393ad3a8e35da228455c71360a
Mukund666 <mukund.choudhary14@gmail.com>
2022-06-11 12:37:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -131,6 +131,13 @@ plt.savefig("output.jpg")
 
 
 #Streamlit GUI starts from here
+st.set_page_config(
+	layout="centered",  # Can be "centered" or "wide". In the future also "dashboard", etc.
+	initial_sidebar_state="collapsed",  # Can be "auto", "expanded", "collapsed"
+	page_title=None,  # String or None. Strings get appended with "• Streamlit". 
+	page_icon=None,  # String, anything supported by st.image, or None.
+)
+
 a=st.sidebar.radio('Navigation',['Farm Information','Farmer Data'])
 # df = pd.read_csv("Trebirth.csv")
 
