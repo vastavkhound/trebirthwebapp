fa1729133b51f00968c7c293e2305bf65c198a50
Mukund666 <mukund.choudhary14@gmail.com>
2022-06-11 13:42:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ def Calculate_STFT2(sig_data):
      x= t,
      y= f,
      z= np.real(Zxx),
-     colorscale = 'Hot'	     
+     colorscale = 'Hot',	     
      )]
      layout = go.Layout(
      title = 'STFT',
