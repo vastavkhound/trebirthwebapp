749edd30a3d577a48068f11478d84112570a9eeb
Mukund666 <mukund.choudhary14@gmail.com>
2022-06-11 13:11:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -98,8 +98,8 @@ def Calculate_STFT2(sig_data):
      fs = 100
      amp = 2 * np.sqrt(2)
      f, t, Zxx = signal.stft(sig_data, fs)
-     print(np.real(Zxx))	
-     fig = go.Figure(data=[go.Mesh3d(x=t, y=f, z=np.real(Zxx), color='red', opacity=0.50)])
+     print(np.abs(Zxx))	
+     fig = go.Figure(data=[go.Mesh3d(x=t, y=f, z=np.abs(Zxx), color='red', opacity=0.50)])
      st.plotly_chart(fig, use_container_width=False, sharing="streamlit")
   
   
