cce9e4606c5ef995b5683a7c1458e89a0aa3dc29
Mukund666 <mukund.choudhary14@gmail.com>
2022-06-12 12:51:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -153,7 +153,7 @@ def Calculate_FFT(sig_data):
    #plt.ylabel("Amplitude")
    #st.pyplot(fig)
    yf = yf[:40000]
-   fig = px.line(x=xf, y=np.abs(yf), labels={'x':'Frequency(Hz)', 'y':'Amplitude'},title='Fourier Transform', width = 1000, height = 600, markers=True)
+   fig = px.line(x=xf[:30], y=np.abs(yf)[:70000], labels={'x':'Frequency(Hz)', 'y':'Amplitude'},title='Fourier Transform', width = 1000, height = 600, markers=True)
    fig.update_traces(line_width=1.5)
    st.plotly_chart(fig, use_container_width=False, sharing="streamlit")
    #plt.plot(xf, yf)
