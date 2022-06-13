272976285e899adc585cc5276087528ddb64e701
Mukund666 <mukund.choudhary14@gmail.com>
2022-06-13 15:37:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -225,7 +225,7 @@ if generate_graph_button:
 	st.write("Graphs Generated!")
 	filtered_array = Apply_Filter(Np_array)
 	Plot_Graph(filtered_array)
-	st.write(np.squeeze(Np_array))
+	st.write(np.squeeze(Np_array).shape)
 	st.write(Np_array.shape)
 	Calculate_FFT(Np_array)
 	Calculate_DCT(Np_array)
