ab45c9212bf9cfb989f1b3ffe32d2a7f202f810b
Mukund666 <mukund.choudhary14@gmail.com>
2022-06-13 13:33:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -243,6 +243,10 @@ st.download_button(
      key='download-csv'
  )
 
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+     dataframe = pd.read_csv(uploaded_file)
+     st.write(dataframe)
 
 
 
