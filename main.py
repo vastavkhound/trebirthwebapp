04b1127b0c10b20c96c8e4e18791c60581ae214e
Mukund666 <mukund.choudhary14@gmail.com>
2022-07-19 19:42:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -130,11 +130,12 @@ def Calculate_Phase_Spectrum(sig_data):
   
   
 def Calculate_FFT(sig_data):
-   N = 1500
+   
    yf = rfft(sig_data[:1500])
+   N = yf.size
    xf = rfftfreq(N, 0.01)
    #yf = yf[:60000]
-   fig = px.line(x=xf, y=np.abs(yf), labels={'x':'Frequency(Hz)', 'y':'Amplitude'},title='Fourier Transform', width = 1000, height = 600, markers=True)
+   fig = px.line(x=xf[], y=np.abs(yf), labels={'x':'Frequency(Hz)', 'y':'Amplitude'},title='Fourier Transform', width = 1000, height = 600, markers=True)
    fig.update_traces(line_width=1.5)
    fig.update_layout(yaxis_range=[0,120000])
    fig.update_layout(xaxis_range=[0,20])
